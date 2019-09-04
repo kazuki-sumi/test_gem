152511df5df507d5ee187ed87ae9da1fdcb04386
鷲見和希 <cs5200@icloud.com>
2019-09-04 22:24:01 +0900

    fix

--- lib/gem_sample_pruby.rb
+++ lib/gem_sample_pruby.rb
@@ -1,6 +1,7 @@
 require "gem_sample_pruby/version"
 
 module GemSamplePruby
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.hello_pruby
+    p "hello pruby!!"
+  end
 end
